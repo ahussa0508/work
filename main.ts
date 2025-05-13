3b74fe93f6d969bef49e3720fefdee5d041135fb
ahussa0508 <ahussa0508@bealhighschool.co.uk>
2025-05-13 10:43:54 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+})
+basic.forever(function () {
+	
+})
